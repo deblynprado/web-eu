727dcc6e491ca416bf61f21c4b4f18e0cc2c089c
Deblyn Prado <deblynprado@hotmail.com>
2022-03-28 12:16:12 -0300

    Adding content to calc

--- calc.php
+++ calc.php
@@ -1,6 +1,13 @@
 <?php
 $con = "db-connection.php";
 if (file_exists( $con ) ) :
+
+  // English Version
+  Here we can have anything specific for European website like calc, discount rules etc
+
+  // Spanish Version
+  Aquí podemos tener cualquier cosa específica para el sitio web europeo como calc, reglas de descuento, etc.
+
 else :
   echo "Database connection not found!";
 endif;
