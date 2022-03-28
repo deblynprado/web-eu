cdea533e4a84056d560816d20142abbf1506991b
Deblyn Prado <deblynprado@hotmail.com>
2022-03-28 12:14:34 -0300

    Index page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+<head>
+  <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+</head>
+<body>
+  <?php
+    $login = "/common/login.php";
+
+    if (file_exists( $login ) ) :
+      require_once( $login );
+      require_once "calc.php";
+  else :
+
+  endif;
+  ?>
+</body>
